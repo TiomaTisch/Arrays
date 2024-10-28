f142bac5d7c5e383089f0c3f5c77b8b39ac2bc3f
Tioma <arttisch02@gmail.com>
2024-10-28 13:20:51 +0300

    HW2 v2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -14,6 +15,9 @@ public class Main {
         for (int i = 0; i < arrBool.length; i++) {
             arrBool[i] = random.nextBoolean();
         }
+        System.out.println(Arrays.toString(arrInt));
+        System.out.println(Arrays.toString(arrFlo));
+        System.out.println(Arrays.toString(arrBool));
 
         System.out.println("_______Задание 2_______");
 
@@ -26,20 +30,47 @@ public class Main {
 
         System.out.println("_______Задание 3_______");
 
-        System.out.print("arrInt = " + arrInt[2] + ", " + arrInt[1] + ", "+ arrInt[0]);
+        System.out.print("arrInt = ");
+        for (int i = arrInt.length-1; i >=0; i--){
+            if (i< arrInt.length-1){
+                System.out.print(", ");
+            }
+            System.out.print(arrInt[i]);
+        }
         System.out.println();
-        System.out.print("arrFlo = " + arrFlo[2] + ", " + arrFlo[1] + ", "+ arrFlo[0]);
+        System.out.print("arrFlo = ");
+        for (int i = arrFlo.length-1; i >=0; i--){
+            if (i< arrFlo.length-1){
+                System.out.print(", ");
+            }
+            System.out.print(arrFlo[i]);
+        }
         System.out.println();
-        System.out.print("arrBool = " + arrBool[2] + ", " + arrBool[1] + ", "+ arrBool[0]);
+        System.out.print("arrBool = ");
+        for (int i = arrBool.length-1; i >=0; i--){
+            if (i< arrBool.length-1){
+                System.out.print(", ");
+            }
+            System.out.print(arrBool[i]);
+        }
         System.out.println();
 
         System.out.println("_______Задание 4_______");
 
+        boolean isIntArray = true;
         for (int i = 0; i < arrInt.length; i++) {
-            if (arrInt[i] % 2 != 0 ) {
-                arrInt[i] += 1;
+            if (arrInt[i] % 1 != 0) {
+                isIntArray = false;
+                break;
             }
-            System.out.print(arrInt[i]+", ");
+        }
+        if(isIntArray){
+            for (int i = 0; i < arrInt.length; i++) {
+                if(arrInt[i] % 2 != 0) {
+                    arrInt[i] += 1;
+                }
+                System.out.print(arrInt[i] + ", ");
+            }
         }
     }
 }
